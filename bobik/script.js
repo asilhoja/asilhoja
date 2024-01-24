8426a6fefbe1d6c42f34a271b3093f78ac14fe5d
asilhoja <151602078+asilhoja@users.noreply.github.com>
2024-01-24 12:05:10 +0500

    Add files via upload

---
+++ bobik/script.js
@@ -0,0 +1,17 @@
+// let a = 5
+// let b = 10
+
+// function sum() {
+//   if (a + b  == 15) {
+//     console.log('да равны 15');
+//   } else {
+//     console.log('нет не равны 15');
+//   }
+// }
+// sum()
+
+let sayHello = () => {
+    console.log('hello');
+}
+
+sayHello()
