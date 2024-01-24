3aae57725bf55d633720ad961d6db19a28adb000
asilhoja <151602078+asilhoja@users.noreply.github.com>
2024-01-24 11:59:57 +0500

    Add files via upload

---
+++ flood/script.js
@@ -0,0 +1,37 @@
+
+alert('ASILXOJA')
+
+
+
+console.log(prompt('сколько тебе лет?') );
+
+let a = 2
+let b = 3
+
+console.log(prompt(''));
+
+let vopros = prompt('напиши любое число')
+
+if(vopros == '22') {
+    console.log('поздровляю ты совпал');
+} else{
+    console.log('ты проволил');
+}
+
+let b = 'asilxoja'
+
+console.log(b + ' is awesome!');
+
+let a = 5
+let b = 7
+
+console.log(a = b);
+
+
+let a = 'standoff'
+let b = 'cs go'
+
+console.log(a == b);
+
+console.log(prompt('напиши свое год рождения'))
+
